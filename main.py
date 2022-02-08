b85a014547f0f28089028c3157e9cb780e5f2042
Semyon Likhorad <63965069+Revcy@users.noreply.github.com>
2022-02-08 16:54:35 +0300

    Create main.py
    Added first code

---
+++ main.py
@@ -0,0 +1,14 @@
+# Classic Calculator on Python 3
+# Developer: github.com/Revcy
+# First big practic project
+
+print('Classic Calculator')
+print('Enter there: ', end='\n')
+
+# + command
+firstNumber = int(input())
+print('+')
+secondNumber = int(input())
+print('=')
+summa = firstNumber + secondNumber
+print(summa)

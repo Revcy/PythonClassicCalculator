bcb459da9387636f3bf51a23da3aca8cadcdca1f
Semyon Likhorad <63965069+Revcy@users.noreply.github.com>
2022-03-10 15:48:31 +0300

    Update main.py
    Changed Data Var Type. Compress code

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 # Classic Calculator on Python 3
-# Developer: github.com/Revcy
-# First big practice project
 
 print('Classic Calculator')
 print('')
 func_choice = str(input('Select Function (+; -; *; /): '))
+num1, num2, = float(input('First Value: ')), float(input('Second Value: '))
 
 # Four math functions
 if func_choice == '+':
-    num1, num2, = int(input('First Value: ')), int(input('Second Value: '))
     print(f'{num1} + {num2} = {num1 + num2}')
 elif func_choice == '-':
-    num1, num2, = int(input('First Value: ')), int(input('Second Value: '))
     print(f'{num1} - {num2} = {num1 - num2}')
 elif func_choice == '*':
-    num1, num2, = int(input('First Value: ')), int(input('Second Value: '))
     print(f'{num1} * {num2} = {num1 * num2}')
 elif func_choice == '/':
-    num1, num2, = int(input('First Value: ')), int(input('Second Value: '))
     if num2 != 0:
         print(f'{num1} / {num2} = {num1 / num2}')
     else:

824d457ca92a7b4bc3f8252b277c790a31ab70a4
Semyon Likhorad <63965069+Revcy@users.noreply.github.com>
2022-03-16 23:32:51 +0300

    Updated Calculator
    Ended calculator :)

--- main.py
+++ main.py
@@ -2,20 +2,34 @@
 
 print('Classic Calculator')
 print('')
-func_choice = str(input('Select Function (+; -; *; /): '))
-num1, num2, = float(input('First Value: ')), float(input('Second Value: '))
 
-# Four math functions
-if func_choice == '+':
-    print(f'{num1} + {num2} = {num1 + num2}')
-elif func_choice == '-':
-    print(f'{num1} - {num2} = {num1 - num2}')
-elif func_choice == '*':
-    print(f'{num1} * {num2} = {num1 * num2}')
-elif func_choice == '/':
-    if num2 != 0:
-        print(f'{num1} / {num2} = {num1 / num2}')
+while True:
+    func_choice = input('Select Function (+; -; *; /): ')
+    num1, num2, = float(input('First Value: ')), float(input('Second Value: '))
+
+    if func_choice == '+':
+        print(f'{num1} + {num2} = {num1 + num2}')
+    elif func_choice == '-':
+        print(f'{num1} - {num2} = {num1 - num2}')
+    elif func_choice == '*':
+        print(f'{num1} * {num2} = {num1 * num2}')
+    elif func_choice == '/':
+        if num2 != 0:
+            print(f'{num1} / {num2} = {num1 / num2}')
+        else:
+            print("Math error: Don't split to zero")
     else:
-        print("Math error: Don't split to zero")
-else:
-    print("Script error: Symbol operation not found")
+        print("Script error: Symbol operation not found")
+    while True:
+        quit_promt = input('You wanna exit? (Yes, No): ')
+        if quit_promt == 'Yes':
+            quit_flag = True
+            break
+        elif quit_promt == 'No':
+            quit_flag = False
+            break
+        else:
+            print('Unknown input. Try again: ')
+    if quit_flag:
+        break
+    
